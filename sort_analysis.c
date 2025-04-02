21765ebb9a775077b5ea59bb9cba83152134d25e
madgallego <mharc.gallego@gmail.com>
2025-04-02 12:56:51 +0800

    UPD: added instructions iin the c program for analysis

---
+++ sort_analysis.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+//make use of fucntion decalrations here, and function definitions in below the main function
+
+
+void main() {
+    //user inputs will also occur here
+    //add the calls here
+    //use swtich case
+
+
+
+
+
+
+}
+
+
+//add the different sorting algorithms beyond this point
